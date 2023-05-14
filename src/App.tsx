fa766226c8091754b2b2cadd34bc800d6705d493
Ben L <veloswim@gmail.com>
2023-05-14 09:29:29 -0700

    added basic socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import "./App.css";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const clientSocket = io("http://localhost:3001");
 
 function App() {
+  useEffect(() => {
+    clientSocket.on("connect", () => {
+      console.log("Connected to the server");
+    });
+
+    clientSocket.on("disconnect", () => {
+      console.log("Disconnected from the server");
+    });
+  });
   return (
     <div className="App">
       <p>chat-time</p>
