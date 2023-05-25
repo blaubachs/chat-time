ddc6458c3ede514b01622c9178f65c70141f2a58
Ben L <veloswim@gmail.com>
2023-05-25 10:08:29 -0700

    added stats component

--- src/components/CharacterMenu.tsx
+++ src/components/CharacterMenu.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Stats from "./Stats";
 
 export default function CharacterMenu() {
   return (
     <div className="border-2 border-blue-400 rounded-md h-full">
       <p>Name</p>
       <div className="flex justify-around">
-        <div>Stats</div>
+        {/* Use a new reusable components for char stats */}
+        <div>
+          <p>Stats</p>
+          <div>
+            <Stats />
+          </div>
+        </div>
         <div>image</div>
       </div>
     </div>
---
+++ src/components/Stats.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Stats() {
+  return (
+    <div>
+      <ol className="flex-col text-left border-2 border-blue-400 p-1">
+        <li>Health</li>
+        <li>Mana</li>
+        <li>Strength</li>
+        <li>Magic Ability</li>
+        <li>Endurance</li>
+        <li>Charisma</li>
+      </ol>
+    </div>
+  );
+}
