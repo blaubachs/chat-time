86b508b4a6bd0031224cc6f5a797d3c3ffef23f4
Ben L <veloswim@gmail.com>
2023-06-04 14:41:24 -0700

    adjustments to login inputs

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -29,19 +29,22 @@ export default function Login({ setToken, setUserObject }: LoginSignupProps) {
       <form className="flex flex-col" onSubmit={handleLoginSubmit}>
         <label htmlFor="username">Username</label>
         <input
-          className="m-1 text-black"
+          className="m-1 text-black rounded-md"
           name="username"
+          autoComplete="off"
           onChange={handleLoginFormChange}
           value={loginForm.username}
         />
         <label htmlFor="password">Password</label>
         <input
-          className="m-1 text-black"
+          className="m-1 text-black rounded-md"
           name="password"
+          type="password"
+          autoComplete="off"
           onChange={handleLoginFormChange}
           value={loginForm.password}
         />
-        <button className="m-3 p-1 bg-blue-400" type="submit">
+        <button className="m-3 p-1 bg-blue-400 rounded-md" type="submit">
           Login
         </button>
       </form>
